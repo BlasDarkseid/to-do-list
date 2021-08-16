39fbb80351869e89c3fc1316acb55e6c5aeb8fbc
Blas <blasgonzalez2414@gmail.com>
2021-08-16 14:13:52 -0400

    CSS agregado

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/header';
 import { Footer } from "./components/footer";
-import {List} from './components/list'
+import { List } from './components/list'
 
 
  const App = () => {
